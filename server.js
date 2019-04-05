3edfa448c3b68a52c534cfac7d01e359c73ce3c1
Dougherty <ryan.dougherty@ert.com>
2019-04-05 20:57:07 +0100

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-var express = require('express');
+const express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
-var backend = express();
+const backend = express();
+
+backend.use(bodyParser.urlencoded({ extended: false }));
+backend.use(bodyParser.json());
+
+const db = require('./server/config/keys').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connection successful'))
+    .catch(err => console.error(err));
 
 backend.listen(3001, () => console.log('lb-server listening on port 3001'));
---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://ziggy:moonagedaydream@cluster0-bj0oi.mongodb.net/test?retryWrites=true'
+};
