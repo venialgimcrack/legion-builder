14cd2f80c1b1e1788c32e8d5269be0163b23bf46
Dougherty <ryan.dougherty@ert.com>
2019-04-08 18:22:21 +0100

    refactored Home

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,27 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const LoggedIn = () => {
-    return (
-        <div>
-            <ul>
-                <li><Link to="/lists">Lists</Link></li>
-                <li><Link to="/collection">Collection</Link></li>
-            </ul>
-        </div>
-    );
-};
+const LoggedIn = () => (
+    <div>
+        <ul>
+            <li><Link to="/lists">Lists</Link></li>
+            <li><Link to="/collection">Collection</Link></li>
+        </ul>
+    </div>
+);
 
-const LoggedOut = () => {
-    return (
-        <div>
-            <ul>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
-            </ul>
-        </div>
-    );
-};
+const LoggedOut = () => (
+    <div>
+        <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+        </ul>
+    </div>
+);
 
 const Home = ({ isAuthenticated }) => {
     return isAuthenticated ? <LoggedIn /> : <LoggedOut />;
