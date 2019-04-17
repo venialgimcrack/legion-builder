76e5efcfc245ab4529e8522642f6ec1b82cdf888
Dougherty <ryan.dougherty@ert.com>
2019-04-17 17:06:19 +0100

    experimental collection page UI

--- client/src/Collection.js
+++ client/src/Collection.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+// import Checkbox from '@material-ui/core/Checkbox';
+import Divider from '@material-ui/core/Divider';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+// import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+// import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import { loadCollection, saveCollection } from './actions/collectionActions';
 import { getProducts } from './actions/productActions';
 
@@ -25,6 +41,12 @@ class Collection extends Component {
             collection: this.props.collection.slice()
         };
     }
+
+    onExpand = panel => (event, expanded) => {
+        this.setState({
+            expanded: expanded ? panel : false
+        });
+    };
 
     onChange = e => {
         let product = e.target.id,
@@ -52,25 +74,67 @@ class Collection extends Component {
     }
 
     render () {
-        return (
-            <div>
-                <div>Collection</div>
-                <form noValidate onSubmit={this.onSubmit}>
-                    {
-                        this.props.products.map((product, i) => {
-                            let inputKey = `check_prod${i}`,
-                                checked = this.state.collection.indexOf(product._id) !== -1;
+        const { classes } = this.props,
+            { expanded } = this.state;
 
-                            return (
-                                <div key={inputKey}>
-                                    <input id={product._id} type="checkbox" onChange={this.onChange} checked={checked} />
-                                    <label htmlFor={product._id}>{product.name}</label>
-                                </div>
-                            );
-                        })
-                    }
-                    <button type="submit">Save</button>
-                </form>
+        return (
+            <div className={classes.root}>
+                <ExpansionPanel expanded={expanded === 'core'} onChange={this.onExpand('core')}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography className={classes.heading}>
+                            Core Sets
+                        </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <form noValidate>
+                            <List dense className={classes.list}>
+                            {
+                                this.props.products.filter(product => product.category === 'core').map((product, index) => {
+                                    let itemKey = `prod_core_${index}`;
+
+                                    return (
+                                        <ListItem key={itemKey} alignItems="flex-start">
+                                            <ListItemText primary={product.name} />
+                                        </ListItem>
+                                    );
+                                })
+                            }
+                            </List>
+                        </form>
+                    </ExpansionPanelDetails>
+                    <Divider />
+                    <ExpansionPanelActions>
+                        <Button size="small" color="primary">Save</Button>
+                    </ExpansionPanelActions>
+                </ExpansionPanel>
+                <ExpansionPanel expanded={expanded === 'xpacks'} onChange={this.onExpand('xpacks')}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography className={classes.heading}>
+                            Expansions
+                        </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <form noValidate>
+                            <List dense className={classes.list}>
+                            {
+                                this.props.products.filter(product => product.category === 'expansion').map((product, index) => {
+                                    let itemKey = `prod_xpack_${index}`;
+
+                                    return (
+                                        <ListItem key={itemKey} alignItems="flex-start">
+                                            <ListItemText primary={product.name} />
+                                        </ListItem>
+                                    );
+                                })
+                            }
+                            </List>
+                        </form>
+                    </ExpansionPanelDetails>
+                    <Divider />
+                    <ExpansionPanelActions>
+                        <Button size="small" color="primary">Save</Button>
+                    </ExpansionPanelActions>
+                </ExpansionPanel>
             </div>
         );
     }
@@ -89,4 +153,15 @@ const mapDispatchToProps = {
     saveCollection
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Collection);
+const styles = theme => ({
+    root: {
+        width: 'auto',
+        margin: theme.spacing.unit
+    },
+    list: {
+        width: '100%'
+    }
+});
+
+const connected = connect(mapStateToProps, mapDispatchToProps)(Collection);
+export default withStyles(styles)(connected);
