d8258e5dcf86677add18b413ff1c24fb4e38a0d3
Dougherty <ryan.dougherty@ert.com>
2019-07-09 16:00:33 -0400

    experimental rank filtering

--- client/src/utils/collectionCalculator.js
+++ client/src/utils/collectionCalculator.js
@@ -2,23 +2,31 @@ import _ from 'lodash';
 
 import store from '../store';
 
-export const getOwnedItems = (group, draft = true) => {
-    const { collection, products } = store.getState();
+export const getOwnedItems = (group, draft = true, rank = false) => {
+    const { collection, products, content } = store.getState();
 
     // Don't try to calculate if data is still loading.
     if (collection.loading || products.loading) {
         return [];
     }
 
-    let part = draft ? 'draft' : 'saved';
+    let part = draft ? 'draft' : 'saved',
+        results = {};
 
     if (group === 'products') {
-        return _.get(collection, `[${part}].products`, []);
+        collection[part].products.forEach(product => results[product.id] = product.count);
+        return results;
     }
 
     let productItems = products.items,
-        results = [],
-        modifiers = _.get(collection, `[${part}][${group}]`, []);
+        allItems = content[group],
+        modifiers = _.get(collection, `[${part}][${group}]`, []),
+        // TODO come up with better rank filtering mechanism
+        rankFilter = thing => {
+            if (!rank) { return true; }
+            let item = allItems.find(item => item.id === thing.id);
+            return item && item.rank === rank;
+        };
 
     // Compose list with raw counts first
     collection[part].products.forEach(product => {
@@ -26,38 +34,28 @@ export const getOwnedItems = (group, draft = true) => {
             prodContents = _.get(fullProd, `contents[${group}]`, []),
             prodCount = product.count;
 
-        prodContents.forEach(item => {
-            let result = results.find(res => res.id === item.id);
-
-            if (result) {
-                result.count += (item.count * prodCount);
-            } else {
-                results.push({
-                    id: item.id,
-                    count: item.count * prodCount
-                });
+        prodContents.filter(rankFilter).forEach(item => {
+            if (!results[item.id]) {
+                results[item.id] = 0;
             }
+
+            results[item.id] += (item.count * prodCount);
         });
     });
 
     // Iterate over and apply modifier values
-    modifiers.forEach(modder => {
-        let item = results.find(res => res.id === modder.id);
+    modifiers.filter(rankFilter).forEach(modder => {
+        if (!results[modder.id] && modder.modifier !== 0) {
+            results[modder.id] = modder.modifier;
 
-        if (item && modder.modifier !== 0) {
-            item.count += modder.modifier;
-
-        } else {
-            results.push({
-                id: modder.id,
-                count: modder.modifier
-            });
+        } else if (modder.modifier !== 0) {
+            results[modder.id] += modder.modifier;
         }
     });
 
-    return results.filter(result => result.count > 0);
+    return _.pickBy(results, result => result > 0);
 };
 
 // Convenience shortcuts for use in the unit/upgrade list panels.
-export const getOwnedUnits = () => getOwnedItems('units', false);
-export const getOwnedUpgrades = () => getOwnedItems('upgrades', false);
+export const getOwnedUnits = rank => getOwnedItems('units', false, rank);
+export const getOwnedUpgrades = rank => getOwnedItems('upgrades', false, rank);
